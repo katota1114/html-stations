ee625b1eb459f5e8c2d367960b46d2ffa8125588
katota1114 <katota1114@gmail.com>
2023-08-18 20:09:51 +0900

    [add] 最終コード

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,27 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await test(userList)
   return await result
 }
 
-function test() {
-  return
+function test(dataList) {
+  return new Promise((resolve)=>{setTimeout(() => {
+    resolve(dataList.map(buildFullName));
+  }, 3000);
+})
 }
 
+function buildFullName(data) {
+    // Station14の問題はこちらです。想定する答えをここを書いてください。
+    // 氏名がわかるようにしつつ、半角スペースで繋いでください。
+    // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
+    return {
+        id: data.id,
+        full_name: data.family_name + ' ' + data.first_name,
+        first_name: data.first_name,
+        family_name: data.family_name,
+        affiliation: data.affiliation,
+        is_student: data.is_student
+    }
+}
